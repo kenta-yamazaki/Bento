197967b53c245f5753f6e918e4666dba5704c7d3
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-07-03 15:44:47 +0900

    不要なコードの削除

---
+++ src/main/java/com/Bento/application/BentoApplication.java
@@ -0,0 +1,12 @@
+package com.Bento.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BentoApplication.class, args);
+    }
+}
