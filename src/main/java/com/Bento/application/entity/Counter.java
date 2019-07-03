16fca9f610ac6ae968682cec6dfd39703f38c335
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-07-03 15:38:26 +0900

    Lombokを使用してゲッターを削除 テーブル名をキャメルケースに修正 import分を修正 パッケージ名の修正 idをint型に修正

---
+++ src/main/java/com/Bento/application/entity/Counter.java
@@ -0,0 +1,31 @@
+package com.Bento.application.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "testtable")
+public class Counter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+}
