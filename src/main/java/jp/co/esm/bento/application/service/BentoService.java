2a3160ffae3fd2eb3e542d88ade92662301aa706
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-07-08 13:23:27 +0900

    NoSuchElementExceptionの明示による修正

--- src/main/java/jp/co/esm/bento/application/service/BentoService.java
+++ src/main/java/jp/co/esm/bento/application/service/BentoService.java
@@ -1,12 +1,14 @@
 package jp.co.esm.bento.application.service;
 
-import jp.co.esm.bento.application.entity.Test;
+import jp.co.esm.bento.application.entity.BentoOrder;
 import jp.co.esm.bento.application.repository.BentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.sql.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -14,20 +16,28 @@ public class BentoService {
 
     private final BentoRepository repository;
 
-    public List<Test> selectAll() {
-        List<Test> list = repository.findAll();
+    public List<BentoOrder> selectAll() {
+        List<BentoOrder> list = repository.findAll();
         return list;
     }
 
-    public Test selectData(String id) {
-        Optional<Test> list = repository.findById(Integer.parseInt(id));
+    public BentoOrder selectData(String id) throws NoSuchElementException {
+        Optional<BentoOrder> list = repository.findById(Integer.parseInt(id));
+        if(!list.isPresent()){
+            throw new NoSuchElementException("指定のIDはありません。");
+        }else
         return list.get();
     }
 
     public void bentoInsert() {
-        Test test = new Test();
-        test.setName("testName");
-        test.setAge(34);
-        repository.save(test);
+        BentoOrder bentoOrder = new BentoOrder();
+        Date date = Date.valueOf("2019-07-07");
+        bentoOrder.setOrder_date(date);
+        bentoOrder.setOrder_name("名無し");
+        bentoOrder.setOrder_menu(1);
+        bentoOrder.setOrder_rice(1);
+        date.valueOf("2019-07-10");
+        bentoOrder.setOrder_arrival(date);
+        repository.save(bentoOrder);
     }
 }
