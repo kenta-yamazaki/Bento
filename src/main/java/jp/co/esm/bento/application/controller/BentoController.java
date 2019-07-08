95a6018b892fd687f5db2b54d28391af4f6bde92
kenta-yamazaki <kenta.yamazaki@esm.co.jp>
2019-07-08 13:16:54 +0900

    NoSuchElementExeceptionを明示

--- src/main/java/jp/co/esm/bento/application/controller/BentoController.java
+++ src/main/java/jp/co/esm/bento/application/controller/BentoController.java
@@ -1,11 +1,15 @@
 package jp.co.esm.bento.application.controller;
 
+import jp.co.esm.bento.application.entity.BentoOrder;
 import jp.co.esm.bento.application.service.BentoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,13 +19,19 @@ public class BentoController {
     private final BentoService bentoService;
 
     @RequestMapping("/all")
-    public void selectAll() {
-        bentoService.selectAll();
+    public List<BentoOrder> selectAll() {
+        return bentoService.selectAll();
     }
 
     @RequestMapping("/select")
-    public void select(@RequestParam("id") String id) {
-        bentoService.selectData(id);
+    public Object test(@RequestParam("id") String id) {
+        BentoOrder result = null;
+        try {
+            result = bentoService.selectData(id);
+        } catch (NoSuchElementException e) {
+            return e.getMessage();
+        }
+        return result;
     }
 
     @PostMapping("/insert")
