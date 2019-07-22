5b871d78933318c3910b6105887044d47537d347
reika-takamiya <reika.takamiya@esm.co.jp>
2019-07-22 13:50:02 +0900

    全員の注文履歴を見ることができる機能を追加

--- src/main/java/jp/co/esm/bento/application/controller/BentoController.java
+++ src/main/java/jp/co/esm/bento/application/controller/BentoController.java
@@ -3,39 +3,39 @@ package jp.co.esm.bento.application.controller;
 import jp.co.esm.bento.application.entity.BentoOrder;
 import jp.co.esm.bento.application.service.BentoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.NoSuchElementException;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/bento")
 public class BentoController {
-
     private final BentoService bentoService;
 
-    @RequestMapping("/all")
-    public List<BentoOrder> selectAll() {
-        return bentoService.selectAll();
+    @RequestMapping(value = "/user/order", method = RequestMethod.GET)
+    public String displayAdd(Model model) {
+        model.addAttribute("bentoOrder", new BentoOrder());
+        return "order";
     }
 
-    @RequestMapping("/select")
-    public Object test(@RequestParam("id") String id) {
-        BentoOrder result = null;
-        try {
-            result = bentoService.selectData(id);
-        } catch (NoSuchElementException e) {
-            return e.getMessage();
+    @RequestMapping(value = "/user/orderHistory", method = RequestMethod.POST)
+    public String create(@Validated @ModelAttribute BentoOrder bentoOrder, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "order";
+        } else {
+            model.addAttribute("bentoOrder", bentoOrder);
+            bentoService.create(bentoOrder);
+            return "orderHistory";
         }
-        return result;
     }
-
-    @PostMapping("/insert")
-    public void insert() {
-        bentoService.bentoInsert();
+    @RequestMapping(value = "/user/orderList", method = RequestMethod.GET)
+    public String displayList(Model model){
+        model.addAttribute("bentoOrderList", bentoService.selectAll());
+        return "orderList";
     }
 }
