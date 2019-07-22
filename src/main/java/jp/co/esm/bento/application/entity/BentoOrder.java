a9e91db378cb41fd3b398dcfee6803f25a6ec0f4
reika-takamiya <reika.takamiya@esm.co.jp>
2019-07-22 10:14:02 +0900

    order_dateを削除 arrival_dateをString型に変更

--- src/main/java/jp/co/esm/bento/application/entity/BentoOrder.java
+++ src/main/java/jp/co/esm/bento/application/entity/BentoOrder.java
@@ -1,38 +1,24 @@
 package jp.co.esm.bento.application.entity;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Column;
 import javax.persistence.GenerationType;
-
+import javax.persistence.Column;
 import javax.validation.constraints.NotEmpty;
 
 
-
 @Entity
-//@AllArgsConstructor
-//@NoArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "bento_order")
 @Data
 public class BentoOrder {
-
-    public BentoOrder(){
-        super();
-    }
-
-    public BentoOrder(Integer id,String name,Integer bentoId,Integer riceId,String arrivalDate){
-        super();
-        this.id = id;
-        this.name = name;
-        this.bentoId = bentoId;
-        this.riceId = riceId;
-        this.arrivalDate = arrivalDate;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -43,52 +29,11 @@ public class BentoOrder {
     private String name;
 
     @Column(name = "bento_id")
-    private Integer bentoId;
+    private Integer bento_id;
 
     @Column(name = "rice_id")
-    private Integer riceId;
+    private Integer rice_id;
 
     @Column(name = "arrival_date")
-    private String arrivalDate;
-
-
-//       public Integer getId(){
-//        return this.id;
-//    }
-//
-//    public String getName() {
-//        return this.name;
-//    }
-//
-//    public Integer getBento_id() {
-//        return this.bentoId;
-//    }
-//
-//    public Integer getRice_id() {
-//        return this.riceId;
-//    }
-//
-//    public String getArrival_date() {
-//        return this.arrivalDate;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public void setBento_id(Integer bento_id) {
-//        this.bento_id = bento_id;
-//    }
-//
-//    public void setRice_id(Integer rice_id) {
-//        this.rice_id = rice_id;
-//    }
-//
-//    public void setArrival_date(String arrival_date) {
-//        this.arrival_date = arrival_date;
-//    }
+    private String arrival_date;
 }
