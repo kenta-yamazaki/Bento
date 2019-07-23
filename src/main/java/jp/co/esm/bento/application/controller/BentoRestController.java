3752f4669f85cd2ca27a900692e96d61ae994de2
reika-takamiya <reika.takamiya@esm.co.jp>
2019-07-23 12:02:14 +0900

    RestControllerクラスを作成

---
+++ src/main/java/jp/co/esm/bento/application/controller/BentoRestController.java
@@ -0,0 +1,34 @@
+package jp.co.esm.bento.application.controller;
+
+import jp.co.esm.bento.application.entity.BentoOrder;
+import jp.co.esm.bento.application.service.BentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/bento")
+public class BentoRestController {
+    private final BentoService bentoService;
+
+    @RequestMapping("/all")
+    public List<BentoOrder> selectAll() {
+        return bentoService.selectAll();
+    }
+
+    @RequestMapping("/select")
+    public Object selectData(@RequestParam("id") String id) {
+        BentoOrder result = null;
+        try {
+            result = bentoService.selectData(id);
+        } catch (NoSuchElementException e) {
+            return e.getMessage();
+        }
+        return result;
+    }
+}
