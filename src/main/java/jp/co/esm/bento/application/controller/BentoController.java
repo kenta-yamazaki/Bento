0899abec180d545a732f3bdad415aa7f081555ea
reika-takamiya <reika.takamiya@esm.co.jp>
2019-07-23 11:51:12 +0900

    不要なコメントの削除 RestControllerにコードを分割

--- src/main/java/jp/co/esm/bento/application/controller/BentoController.java
+++ src/main/java/jp/co/esm/bento/application/controller/BentoController.java
@@ -5,48 +5,16 @@ import jp.co.esm.bento.application.service.BentoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.util.List;
-import java.util.NoSuchElementException;
-
-
 @Controller
-//@AllArgsConstructor
-//@NoArgsConstructor
 @RequiredArgsConstructor
 @RequestMapping("/bento")
 public class BentoController {
     private final BentoService bentoService;
-
-    @RequestMapping("/all")
-    public List<BentoOrder> selectAll() {
-        return bentoService.selectAll();
-    }
-
-
-
-    @RequestMapping("/select")
-    public Object selectData(@RequestParam("id") String id) {
-        BentoOrder result = null;
-        try {
-            result = bentoService.selectData(id);
-        } catch (NoSuchElementException e) {
-            return e.getMessage();
-        }
-        return result;
-    }
 
     @RequestMapping(value = "/user/order", method = RequestMethod.GET)
     public String displayAdd(Model model) {
