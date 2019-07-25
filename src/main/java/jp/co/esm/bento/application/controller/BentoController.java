50d3ef00ce2dc581b6c89d1703127dd30867def3
reika-takamiya <reika.takamiya@esm.co.jp>
2019-07-25 11:43:53 +0900

    ラジオボタン機能の追加

--- src/main/java/jp/co/esm/bento/application/controller/BentoController.java
+++ src/main/java/jp/co/esm/bento/application/controller/BentoController.java
@@ -1,6 +1,7 @@
 package jp.co.esm.bento.application.controller;
 
 import jp.co.esm.bento.application.entity.BentoOrder;
+import jp.co.esm.bento.application.repository.BentoRepository;
 import jp.co.esm.bento.application.service.BentoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -15,10 +16,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/bento")
 public class BentoController {
     private final BentoService bentoService;
+    private final BentoRepository bentoRepository;
 
     @RequestMapping(value = "/user/order", method = RequestMethod.GET)
     public String displayAdd(Model model) {
         model.addAttribute("bentoOrder", new BentoOrder());
+        model.addAttribute("radioItems", getRadioItems());
         return "order";
     }
 
@@ -28,10 +31,21 @@ public class BentoController {
             return "order";
         } else {
             model.addAttribute("bentoOrder", bentoOrder);
+            model.addAttribute("bentoName", bentoRepository.findById(bentoOrder.getBento_id()).get().getName());
             bentoService.create(bentoOrder);
             return "orderHistory";
         }
     }
+
+    private Map<String, String> getRadioItems() {
+        Map<String, String> selectMap = new LinkedHashMap<String, String>();
+        selectMap.put("0", "勇気");
+        selectMap.put("1", "本気");
+        selectMap.put("2", "元気");
+
+        return selectMap;
+    }
+
     @RequestMapping(value = "/user/orderList", method = RequestMethod.GET)
     public String displayList(Model model){
         model.addAttribute("bentoOrderList", bentoService.selectAll());
