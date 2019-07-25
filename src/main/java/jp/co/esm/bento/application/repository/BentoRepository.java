5067238016178a2d3f66e3e3a2ee62bdcbcc0279
reika-takamiya <reika.takamiya@esm.co.jp>
2019-07-25 11:41:43 +0900

    BentoテーブルのRepositoryクラスを作成

--- src/main/java/jp/co/esm/bento/application/repository/BentoRepository.java
+++ src/main/java/jp/co/esm/bento/application/repository/BentoRepository.java
@@ -1,10 +1,8 @@
 package jp.co.esm.bento.application.repository;
 
-import jp.co.esm.bento.application.entity.BentoOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BentoRepository extends JpaRepository<BentoOrder, Integer> {
+public interface BentoRepository extends JpaRepository<Bento, Integer> {
 
 }
