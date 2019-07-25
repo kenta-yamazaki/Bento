e3c1713b51c11dc86a6a19827404336daab4ee62
reika-takamiya <reika.takamiya@esm.co.jp>
2019-07-25 15:07:36 +0900

    ご飯の有無を選べるラジオボタンを作成

--- src/main/java/jp/co/esm/bento/application/controller/BentoController.java
+++ src/main/java/jp/co/esm/bento/application/controller/BentoController.java
@@ -1,6 +1,7 @@
 package jp.co.esm.bento.application.controller;
 
 import jp.co.esm.bento.application.entity.BentoOrder;
+import jp.co.esm.bento.application.repository.RiceRepository;
 import jp.co.esm.bento.application.service.BentoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,15 +12,20 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/bento")
 public class BentoController {
     private final BentoService bentoService;
+    private final RiceRepository riceRepository;
 
     @RequestMapping(value = "/user/order", method = RequestMethod.GET)
     public String displayAdd(Model model) {
         model.addAttribute("bentoOrder", new BentoOrder());
+        model.addAttribute("radioRice", getRadioRice());
         return "order";
     }
 
@@ -29,9 +35,19 @@ public class BentoController {
             return "order";
         } else {
             model.addAttribute("bentoOrder", bentoOrder);
+            model.addAttribute("riceAvailablity", riceRepository.findById(bentoOrder.getBento_id()).get().getAvailability());
             bentoService.create(bentoOrder);
             return "orderHistory";
         }
+    }
+
+
+    private Map<String, String> getRadioRice() {
+        Map<String, String> selectMap = new LinkedHashMap<String, String>();
+        selectMap.put("0", "ご飯あり");
+        selectMap.put("1", "ご飯なし");
+
+        return selectMap;
     }
 
     @RequestMapping(value = "/user/orderList", method = RequestMethod.GET)
