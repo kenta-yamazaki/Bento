77f9b0216468a0f8cc7aac24062df55d74dd33a8
reika-takamiya <reika.takamiya@esm.co.jp>
2019-07-26 11:28:32 +0900

    BentoControllerクラスの処理を記述

--- src/main/java/jp/co/esm/bento/application/service/BentoService.java
+++ src/main/java/jp/co/esm/bento/application/service/BentoService.java
@@ -1,19 +1,20 @@
 package jp.co.esm.bento.application.service;
 
+import jp.co.esm.bento.application.entity.Bento;
 import jp.co.esm.bento.application.entity.BentoOrder;
 import jp.co.esm.bento.application.repository.BentoOrderRepository;
+import jp.co.esm.bento.application.repository.BentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.*;
 
 
 @Service
 @RequiredArgsConstructor
 public class BentoService {
     private final BentoOrderRepository bentoOrderRepository;
+    private final BentoRepository bentoRepository;
 
     public List<BentoOrder> selectAll() {
         List<BentoOrder> list = bentoOrderRepository.findAll();
@@ -31,5 +32,23 @@ public class BentoService {
         } else {
             return order.get();
         }
+    }
+
+    public Bento selectBento(Integer id) throws NoSuchElementException {
+        Optional<Bento> bento = bentoRepository.findById(id);
+        if (!bento.isPresent()) {
+            throw new NoSuchElementException("指定したIDはありません。");
+        } else {
+            return bento.get();
+        }
+    }
+
+    public Map<String, String> getRadioItems() {
+        Map<String, String> selectMap = new LinkedHashMap<String, String>();
+        List<Bento> bento = bentoRepository.findAll();
+        for (int i = 0; i < bento.size(); i++) {
+            selectMap.put(String.valueOf(bento.get(i).getId()), bento.get(i).getName());
+        }
+        return selectMap;
     }
 }
